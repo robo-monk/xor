9461d9d9674497203f3d81b8ebd8401364ce9c6e
robo-monk <rrobomonk@gmail.com>
2022-09-14 02:03:25 +0300

    monaco editor

--- src/monaco-editor/MonacoEditor.svelte
+++ src/monaco-editor/MonacoEditor.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
-  import * as monaco from 'monaco-editor'
+  import { monaco } from './monaco';
+  // import * as monaco from 'monaco-editor'
   import { onMount } from 'svelte'
+  import { emit, listen } from '@tauri-apps/api/event'
+
+  // import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+  // import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+  // import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+  // import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+  // import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+
   let container: HTMLElement
 
-  onMount(() => {
+
+  onMount(async () => {
+
     const editor = monaco.editor.create(container, {
       value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
       language: 'javascript',
     })
+
+    monaco.editor.setTheme('vs-dark')
+    // monaco.editor.setTheme("hc-black");
+    console.log('>> ', monaco.editor)
 
     // editor.
 
@@ -17,6 +32,12 @@
       })
     })
   })
+
+  const pressedKeys = new Map()
+
+  addEventListener('keydown', (e) => {})
+
+  addEventListener('keyup', (e) => {})
 </script>
 
 <div>
@@ -25,6 +46,7 @@
 
 <style lang="scss">
   #editor {
+    // display: none;
     height: 100vh;
     width: 100vw;
   }
