d78bf2130f78cb2074cf025aff7d56a0a27995b0
robo-monk <rrobomonk@gmail.com>
2022-09-14 03:10:40 +0300

    read files

--- src/fs/Entry.svelte
+++ src/fs/Entry.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
   export let entry: import('@tauri-apps/api/fs').FileEntry
   import { readDir, BaseDirectory, FileEntry } from '@tauri-apps/api/fs'
+  import { createEventDispatcher } from "svelte";
+
+ 	const dispatch = createEventDispatcher();
 
   let expanded = false;
 
   async function expand() {
-    entry.children = await readDir(entry.path)
+    entry.children = await readDir(entry.path).catch(e => {
+      const event = new CustomEvent("fileOpen", {
+        detail: {
+          file: entry
+        }
+      });
+      openFile(event);
+    }) || []
     expanded = true
   }
 
@@ -13,6 +23,13 @@
     entry.children = []
     expanded = false
   }
+
+  function openFile(event: CustomEvent) {
+    dispatch('openFile', {
+      file: event.detail.file
+    });
+  }
+
 </script>
 
 <div class="entry" >
@@ -24,7 +41,7 @@
   <div class="children">
     {#if entry.children?.length }
       {#each entry.children || [] as entry}
-        <svelte:self {entry} />
+        <svelte:self {entry} on:openFile={openFile}/>
       {/each}
     {/if}
   </div>
--- src/fs/Sidebar.svelte
+++ src/fs/Sidebar.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Entry from './Entry.svelte';
-  import { readDir, BaseDirectory, FileEntry } from '@tauri-apps/api/fs';
+  import { readDir, BaseDirectory, FileEntry, readTextFile } from '@tauri-apps/api/fs';
   // Reads the `$APPDIR/users` directory recursively
   let currentPath = "vl"
   let entries: FileEntry[] = []
@@ -21,12 +21,19 @@
     processEntries(entries)
   }
 
+  async function openFile(event: CustomEvent) {
+    let file: FileEntry = event.detail.file 
+    console.log("file", file);
+    let contents = await readTextFile(file.path);
+    console.log(">>>", contents);
+  }
+
   refresh()
 </script>
 
 <div class='sidebar'>
   {#each entries as entry }
-    <Entry {entry}></Entry>
+    <Entry {entry} on:openFile={openFile}></Entry>
   <!-- <div>
     <span>x</span>
     <span> {entry.name} </span>
