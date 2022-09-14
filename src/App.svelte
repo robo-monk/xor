b21d12033bc7ca9d794358634fc1e696d90838ee
robo-monk <rrobomonk@gmail.com>
2022-09-14 04:12:06 +0300

    tabs

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,49 @@
 <script lang="ts">
   import MonacoEditor from './monaco-editor/MonacoEditor.svelte'
+  import { nanoid } from 'nanoid';
   import Editor from './editor/Editor.svelte'
   import { onMount } from 'svelte'
 
   import { readDir, BaseDirectory, FileEntry, readTextFile } from '@tauri-apps/api/fs'
   import Sidebar from './fs/Sidebar.svelte'
 
-  let paths: string[] = []
+  // let paths: string[] = []
+  interface Tab {
+    file: FileEntry,
+    uuid: string
+  }
+
+  // let files: FileEntry[] = []
+  let tabs: Tab[] = []
 
   async function openFile(event: CustomEvent) {
     let file: FileEntry = event.detail.file
     console.log('file', file)
     let contents = await readTextFile(file.path)
     console.log('>>>', contents)
-    paths = [...paths, file.path]
+    tabs = [...tabs, {
+      file,
+      uuid: nanoid()
+    } as Tab]
+  }
+
+  async function closeTab(tab: Tab) {
+    console.log('close tab', tab);
+    let index = tabs.findIndex(t => tab.uuid == t.uuid)
+    tabs.splice(index, 1);
+    tabs = tabs
+    console.log(tabs)
   }
 </script>
 
 <div class="window-container">
   <Sidebar on:openFile={openFile} />
 
-  {#if paths.length}
-    {#each paths as path}
+  {#if tabs.length}
+    {#each tabs as tab}
       <div class="tab">
-        <MonacoEditor filePath={path} />
+        <div> <span> {tab.file.name} </span> <span on:click={() => closeTab(tab)}> x </span></div>
+        <MonacoEditor filePath={tab.file.path} />
       </div>
     {/each}
   {/if}
